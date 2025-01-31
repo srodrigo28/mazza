4a3ba5bf6ae0317cf5168214c6901b1928f017d5
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2025-01-31 19:44:24 -0300

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { motion } from "framer-motion"
-import Menu from "./motion/page";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen">
@@ -13,17 +12,27 @@ export default function Home() {
                 rotate: [0, 0, 270, 270, 0],
                 borderRadius: ["10%", "10%", "30%", "30%"],
           }}
-          transition={{ ease: "easeOut", duration: 3 }}
-          className="w-72 h-72 bg-zinc-100 absolute"    
+          transition={{ ease: "easeOut", duration: 6 }}
+          className="w-96 h-96 bg-zinc-100 absolute"    
       ></motion.div>
 
       <motion.div drag dragConstraints={{
               top: -50, left: -50, right: 50, bottom: 50
       }}>
-        <h1 className="text-6xl cursor-pointer text-white tracking-wider">Mazza</h1>
+        <h1 className="text-5xl cursor-pointer text-white tracking-wider">Mazzafashion</h1>
       </motion.div>
 
+      <motion.div
+          initial={{ 
+            rotate: 360,
+            opacity: .3,
+          }}
+          animate={{ scale: 2, opacity: 1, rotate: 360 }}
+          // Fade in when the element enters the viewport:
+          whileInView={{ opacity: 1 }}
+      >
       <img src="./zap.png" className="rounded-full w-36 animate-pulse absolute bottom-2 right-2" alt="" />   
+      </motion.div>
    
     </div>
   );
